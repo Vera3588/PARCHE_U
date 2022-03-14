bee40f105f021162eb2b2bf5d86cabd9e9bae8fc
Vera3588 <javeraa@eafit.edu.co>
2022-03-14 00:11:33 -0500

    ParcheU_v0.1

--- PARCHE_U/usuarios/views.py
+++ PARCHE_U/usuarios/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Inicio(request):
-    return render(request, 'inicio.html',{'name':'Cristian d'})
+    return render(request, 'index.html')
+
+def Registro(request):
+    return render(request, 'registro.html')
